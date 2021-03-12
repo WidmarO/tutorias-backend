d82b8d159d9c8d46e6802105b0a355988f894a07
Widmar Raul <171259@unsaac.edu.pe>
2021-03-12 11:57:43 -0500

    fixed not importin db when running the app

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import os
 
 from flask import Flask
 from flask_cors import CORS
-from flask_restful import  Api
+from flask_restful import Api
 from flask_jwt import JWT
 from datetime import timedelta
 
@@ -31,14 +31,15 @@ CORS(app)
 # pass_database = 'GO8LL0RrW418O2aA'
 # url_database = 'localhost'
 # name_database = 'turbos-flask-db'
-# ---------------------------- CLEVER CLOUD DATABASE 
+# ---------------------------- CLEVER CLOUD DATABASE
 type_database = 'mysql'
 user_database = 'ulhu5xo4wswpfeyf'
 pass_database = '7tZdO2gAvsCnUT2x8vr9'
 url_database = 'bxtmrf8q1ibe3wcmvyie-mysql.services.clever-cloud.com'
 name_database = 'bxtmrf8q1ibe3wcmvyie'
 
-sqlalchemy_database_uri = type_database + '://' + user_database + ':' + pass_database + '@' + url_database + '/' + name_database
+sqlalchemy_database_uri = type_database + '://' + user_database + \
+    ':' + pass_database + '@' + url_database + '/' + name_database
 
 app.config['SQLALCHEMY_DATABASE_URI'] = sqlalchemy_database_uri
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -46,22 +47,19 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'widmar'
 api = Api(app)
 
-@app.before_first_request
-def create_tables():
-  db.create_all()
-
 app.config['JWT_AUTH_URL_RULE'] = '/login'
 app.config['JWT_EXPIRATION_DELTA'] = timedelta(seconds=1800)
 
-jwt = JWT(app, authenticate, identity) # /auth
+jwt = JWT(app, authenticate, identity)  # /auth
 
-api.add_resource(ClientList,'/clients')
-api.add_resource(Client,'/client/<string:dni>') # http://127.0.0.1:5000/user/1
+api.add_resource(ClientList, '/clients')
+# http://127.0.0.1:5000/user/1
+api.add_resource(Client, '/client/<string:dni>')
 api.add_resource(UserRegister, '/register')
-api.add_resource(BrandList,'/brands')
+api.add_resource(BrandList, '/brands')
 
 
 if __name__ == '__main__':
-  from db import db 
-  db.init_app(app)  
-  app.run(debug=True)
+    from db import db
+    db.init_app(app)
+    app.run(debug=True)
