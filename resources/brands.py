e3c2f230667e60f69d5095ff52dc765761c0010c
Widmar Raul <171259@unsaac.edu.pe>
2021-03-24 20:01:56 -0500

    change in the body data from brand to id of method delete for brands

--- resources/brands.py
+++ resources/brands.py
@@ -7,8 +7,8 @@ from Req_Parser import Req_Parser
 
 # class Brand(Resource):
 #   parser = Req_Parser()
-#   parser.add_argument('id',esp_attr=True)  
-#   parser.add_argument('brand',str, True)  
+#   parser.add_argument('id',esp_attr=True)
+#   parser.add_argument('brand',str, True)
 
 #   # @jwt_required()
 #   def get(self, brand):
@@ -17,7 +17,7 @@ from Req_Parser import Req_Parser
 #     if brand:
 #       return brand.json()
 #     return {'message': 'brand not found'},404
-  
+
 #   def post(self, brand):
 #     print(request.json)
 #     '''Add or created a new brand in database if already them not exist'''
@@ -37,71 +37,76 @@ from Req_Parser import Req_Parser
 
 #     return brand.json(),201
 
-  # def delete(self, brand):
-  #   '''Delete a brand from database if exist in it'''
-  #   brand = BrandModel.find_by_brand(brand)
-  #   if brand:
-  #     brand.delete_from_db()      
-  #     return brand.json(),200
+# def delete(self, brand):
+#   '''Delete a brand from database if exist in it'''
+#   brand = BrandModel.find_by_brand(brand)
+#   if brand:
+#     brand.delete_from_db()
+#     return brand.json(),200
 
-  #   return {'message': 'Brand not found.'}
- 
-  # def put(self, dni):    
+#   return {'message': 'Brand not found.'}
 
-  #   ans, data = Client.parser.parse_args(dict(request.json))  
-  #   if not ans:
-  #     return data
+# def put(self, dni):
 
-  #   client = ClientModel.find_by_dni(dni)
+#   ans, data = Client.parser.parse_args(dict(request.json))
+#   if not ans:
+#     return data
 
-  #   if client:
-  #     client.update_data(**data)
-      
-  #   else:
-  #     return {'message':"client not found."}
+#   client = ClientModel.find_by_dni(dni)
 
-  #   client.save_to_db()
-  #   client_list = ClientList()
-  #   return client_list.get()
+#   if client:
+#     client.update_data(**data)
+
+#   else:
+#     return {'message':"client not found."}
+
+#   client.save_to_db()
+#   client_list = ClientList()
+#   return client_list.get()
 
 
 class BrandList(Resource):
-  parser = Req_Parser()
-  parser.add_argument('id',esp_attr=True)  
-  parser.add_argument('brand',str, True)  
-  # @jwt_required()  
-  def get(self):    
-    return list(map(lambda x: x.json(), BrandModel.query.all()))
-  
-  def post(self):
+    parser = Req_Parser()
+    parser.add_argument('id', esp_attr=True)
+    parser.add_argument('brand', str, True)
+    # @jwt_required()
 
-    print(request.json)
-    brand = request.json['brand']
-    '''Add or created a new brand in database if already them not exist'''
-    if BrandModel.find_by_brand(brand):
-      return {'message': "A brand: '{}' already exist in data base".format(brand)}
+    def get(self):
+        sort_brands = list(
+            map(lambda x: x.json(), BrandModel.query.all()))
+        sort_brands = sorted(
+            sort_brands, key=lambda x: x[list(sort_brands[0].keys())[0]])
+        print(sort_brands)
+        return sort_brands
 
-    ans,data = BrandList.parser.parse_args(dict(request.json))
-    if not ans:
-      return data
+    def post(self):
 
-    brand = BrandModel(**data)
+        print(request.json)
+        brand = request.json['brand']
+        '''Add or created a new brand in database if already them not exist'''
+        if BrandModel.find_by_brand(brand):
+            return {'message': "A brand: '{}' already exist in data base".format(brand)}
 
-    try:
-      brand.save_to_db()
-    except:
-      return {'message': "An error ocurred adding the brand"}, 500
+        ans, data = BrandList.parser.parse_args(dict(request.json))
+        if not ans:
+            return data
 
-    return brand.json(),201
+        brand = BrandModel(**data)
 
-  def delete(self):
-    
-    brand = request.json['brand']
-    '''Delete a brand from database if exist in it'''
-    brand = BrandModel.find_by_brand(brand)
-    if brand:
-      brand.delete_from_db()      
-      return brand.json(),200
+        try:
+            brand.save_to_db()
+        except:
+            return {'message': "An error ocurred adding the brand"}, 500
 
-    return {'message': 'Brand not found.'}
-  
+        return brand.json(), 201
+
+    def delete(self):
+
+        _id = request.json['id']
+        '''Delete a id from database if exist in it'''
+        brand = BrandModel.find_by_id(_id)
+        if brand:
+            brand.delete_from_db()
+            return brand.json(), 200
+
+        return {'message': 'Brand not found.'}
