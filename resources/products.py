84194c7fd92845c766fa5e277a13d4058b697a08
Widmar Leon <55371129+WidmarO@users.noreply.github.com>
2021-04-17 12:36:48 -0500

    Update products.py

--- resources/products.py
+++ resources/products.py
@@ -53,7 +53,7 @@ class Product(Resource):
 class ProductList(Resource):
     parser = Req_Parser()
     # parser.add_argument('id', esp_attr=True)
-    parser.add_argument('part', str, required=True)
+    parser.add_argument('part_number', str, required=True)
     parser.add_argument('category', str, required=True)
     parser.add_argument('stock', int, required=True)
     parser.add_argument('purchase_price', float)
@@ -70,7 +70,7 @@ class ProductList(Resource):
 
     def post(self):
         print(request.json)
-        part = request.json['part']
+        part_number = request.json['part_number']
         category = request.json['category']
         stock = request.json['stock']
         purchase_price = request.json['purchase_price']
