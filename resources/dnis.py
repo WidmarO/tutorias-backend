23098f247df6ec71b630bcc10bf724bd0ede8263
Widmar Raul <171259@unsaac.edu.pe>
2021-05-18 23:42:22 -0500

    fix problem in dnis resource

--- resources/dnis.py
+++ resources/dnis.py
@@ -9,11 +9,12 @@ class DNI(Resource):
     parser.add_argument('dni', str, required=True)
 
     def post(self):
+        print(request.json)
         dni = request.json['dni']
-        data = {'document': dni}
+        data = {'document': str(dni)}
         url = 'http://api.peruapis.com/v1/dni'
         headers = {'Authorization': 'Bearer udyhV3w2wAqEhrrlKGxVoKwGld4ayO7v9bjOtMyXRziLvaZyJ8liONnItPLA',
                    'Accept': 'application/json'}
         response = requests.post(url, data=data, headers=headers)
-        print(response.json()['data'])
-        return response.json()['data']
+        print(response.json())
+        return response.json()
