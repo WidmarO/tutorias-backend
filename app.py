67d77dd4daa66cab58dc9460d2715517851c1349
WidmarO <171259@unsaac.edu.pe>
2021-08-17 12:57:04 -0500

    basic configuration of flask project

--- app.py
+++ app.py
@@ -10,40 +10,12 @@ from security import authenticate, identity
 from resources.user import UserRegister
 from resources.client import Client, ClientList
 from resources.brands import BrandList
-from resources.aplications import AplicationList
-from resources.motors import MotorList
 from resources.part_numbers import PartNumber, PartNumberList
 from resources.turbo_models import ModelList
 # from resources.catalogue import CatalogueList, Catalogue
-from resources.employees import Employee, EmployeeList
-from resources.categories import CategoryList
-from resources.products import ProductList, Product
-from resources.dnis import DNI
 from resources.turbos import Turbos
 from resources.parts_models import Parts_Models
 from resources.parts_brands import Parts_Brands
-from resources.parts_motors import Parts_Motors
-from resources.parts_aplications import Parts_Aplications
-
-from models.part_number import PartNumberModel
-from models.turbo_model import TurboModel_Model
-from models.brand import BrandModel
-from models.motor import MotorModel
-from models.part_number import PartNumberModel
-from models.aplication import AplicationModel
-from models.part_model import PartModel_Model
-from models.part_brand import PartBrandModel
-from models.part_motor import PartMotorModel
-from models.part_aplication import PartAplicationModel
-# from models.catalogue import CatalogueModel
-from models.aplication import AplicationModel
-from models.category import CategoryModel
-from models.product import ProductModel
-from models.provider import ProviderModel
-from models.purchase import PurchaseModel
-from models.purchase_detail import PurchaseDetailModel
-from models.employee import EmployeeModel
-from models.category import CategoryModel
 
 
 app = Flask(__name__)
@@ -56,10 +28,10 @@ CORS(app)
 # name_database = 'turbos-flask-db'
 # ---------------------------- CLEVER CLOUD DATABASE
 type_database = 'mysql'
-user_database = 'ulhu5xo4wswpfeyf'
-pass_database = '7tZdO2gAvsCnUT2x8vr9'
-url_database = 'bxtmrf8q1ibe3wcmvyie-mysql.services.clever-cloud.com'
-name_database = 'bxtmrf8q1ibe3wcmvyie'
+user_database = 'udwsw0hbqah0nikx'
+pass_database = '6eaqlxBNvKWNGUuRR32M'
+url_database = 'b0du4ayviyfhrlbnjckc-mysql.services.clever-cloud.com'
+name_database = 'b0du4ayviyfhrlbnjckc'
 
 sqlalchemy_database_uri = type_database + '://' + user_database + \
     ':' + pass_database + '@' + url_database + '/' + name_database
