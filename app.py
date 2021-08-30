b90d73aa92eb9e6a5cc359cee1fccf151e8abf95
WidmarO <171259@unsaac.edu.pe>
2021-08-29 20:16:22 -0500

    clear lines of code that don't use

--- app.py
+++ app.py
@@ -13,10 +13,10 @@ from models.user_role import UserRoleModel
 
 from models.coordinator import CoordinatorModel
 from models.tutoring_program import TutoringProgramModel
-# from models.new import NewModel
-# from models.teacher import TeacherModel
-# from models.principal import PrincipalModel
-# from models.tutor import TutorModel
+from models.new import NewModel
+from models.teacher import TeacherModel
+from models.principal import PrincipalModel
+from models.tutor import TutorModel
 from models.student import StudentModel
 
 from models.tutor_student import TutorStudentModel
@@ -31,18 +31,11 @@ from models.description_workshop_attendance import DescriptionWorkshopAttendance
 
 
 from security import authenticate, identity
+from resources.user import UserRegister
+
 from resources.student import StudentList, Student
-# from resources.user import UserRegister
-# from resources.client import Client, ClientList
-# from resources.brands import BrandList
-# from resources.part_numbers import PartNumber, PartNumberList
-# from resources.turbo_models import ModelList
-# from resources.catalogue import CatalogueList, Catalogue
-# from resources.parts_brands import Parts_Brands
 from resources.documentation import Documentation
-# from resources.catalogue import CatalogueList, Catalogue
-# from resources.parts_brands import Parts_Brands
-# from resources.teacher import TeacherList,Teacher
+
 
 
 app = Flask(__name__)
@@ -50,8 +43,8 @@ CORS(app)
 # ----------------------------- LOCAL DATABASE
 type_database = 'mysql'
 user_database = 'root'
-pass_database = 'root' # for wid is toor
-url_database = 'localhost:3307' # for wid is 127.0.0.1
+pass_database = 'toor' # for wid is toor
+url_database = '127.0.0.1' # for wid is 127.0.0.1
 name_database = 'tutoring-system-bd'
 # ---------------------------- CLEVER CLOUD DATABASE
 # type_database = 'mysql'
@@ -75,22 +68,11 @@ jwt = JWT(app, authenticate, identity)  # /auth
 
 # -- RESOURCES OF THE APPLICATION
 api.add_resource(Documentation, '/')
-# api.add_resource(ClientList, '/clients')
-# api.add_resource(Client, '/client/<string:dni>')
-# api.add_resource(UserRegister, '/register')
-#api.add_resource(ModelList, '/models')
-#api.add_resource(BrandList, '/brands')
-# api.add_resource(TeacherList,'/teachers') #endpoint
-# api.add_resource(Teacher,'/teacher/<string:cod_teach>')
-#api.add_resource(PartNumberList, '/parts')
-#api.add_resource(PartNumber, '/part/<int:id>')
-#api.add_resource(Parts_Brands, '/parts_brands/<string:part_number>')
 api.add_resource(Student, '/student/<string:cod_student>')
 api.add_resource(StudentList, '/students')
 
 
 # -- Module that create the tables in the BD
-#-- Module that create the tables in the BD.
 @app.before_first_request
 def create_tables():
     db.create_all()
