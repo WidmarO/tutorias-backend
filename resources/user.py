7662ae2118757bbcf81bf62f8829dd0516801689
WidmarO <171259@unsaac.edu.pe>
2021-09-02 12:22:21 -0500

    added configuration for authentication

--- resources/user.py
+++ resources/user.py
@@ -1,12 +1,13 @@
 from flask_restful import Resource
 from flask import request
+import jwt
 
 from models.user import UserModel
 from Req_Parser import Req_Parser
 
 from flask_jwt_extended import create_access_token
 from flask_jwt_extended import jwt_required
-from flask_jwt_extended import get_jwt_identity
+from flask_jwt_extended import get_jwt
 
 
 
@@ -15,26 +16,29 @@ class UserRegister(Resource):
   parser = Req_Parser()
   parser.add_argument('username', str, True)
   parser.add_argument('password', str, True)
+  parser.add_argument('role', str, True)
   
   def post(self):
     '''Add a user in a db'''
+    # verify if the request data is valid
     ans, data = UserRegister.parser.parse_args(dict(request.json))
     if not ans:
       return data
-
+    # verify if the username already exists
     if UserModel.find_by_username(data['username']):
       return {'message': "A user with that username already exist"}
-    
-    user = UserModel(data['username'], data['password'], 'student')
+    # save the user in the db
+    user = UserModel(data['username'], data['password'], data['role'])
     user.save_to_db()
-
+    # create a token for the user
     access_token = create_access_token(identity=user.username)
-
-    return {'token': access_token, 'data':user.json()}
+    # return token
+    return {'token': access_token}
 
   @jwt_required()
   def get(self):
         return {'message': 'You are logged in'}, 200
+
 
 class Login(Resource):
   parser = Req_Parser()
@@ -52,8 +56,13 @@ class Login(Resource):
       return {'message': 'User does not exist'}, 404
 
     if user.password == data['password']:
-      access_token = create_access_token(identity={'user':user.username, 'role':user.rol, 'id':user.id})
+      access_token = create_access_token({'user':user.username, 'role':user.role, 'id':user.id})
       return {'token': access_token, 'data': user.json()}
     else:
       return {'message': 'Wrong credentials'}, 401
+
+  @jwt_required()
+  def get(self):
+    claims = get_jwt()
+    return {'message': 'You are logged in', 'role':claims['role'], 'username':claims['username']}, 200
 
