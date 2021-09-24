43514a3b22ae2ebe4e4c0d33e5258bea35ab4e4d
WidmarO <171259@unsaac.edu.pe>
2021-09-24 13:14:18 -0500

    change to cloud database

--- run.py
+++ run.py
@@ -8,17 +8,4 @@ db.init_app(app)
 @app.before_first_request
 def create_tables():
     db.create_all()
-    roles = ['coordinator', 'tutor', 'principal', 'student', 'student_helper']
-    for role in roles:
-        try: 
-            _role = RoleModel(role)
-            _role.save_to_db()
-        except:
-            print('One error ocurred when create the roles')
-    try:
-        user = UserModel('admin','admin','coordinator')
-        user.save_to_db()
-    except:
-        print('One error ocurred when create the admin user')
-
 
